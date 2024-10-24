893c929db0142d616bc2419a73323b7dcf11e3df
HamSeungChan <head0618@naver.com>
2024-10-24 21:14:57 +0900

    [feat] 멤버 Controller 구현

---
+++ backend/src/main/java/sillim/dongnae/member/controller/MemberController.java
@@ -0,0 +1,88 @@
+package sillim.dongnae.member.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import sillim.dongnae.member.dto.request.EmailCheckRequest;
+import sillim.dongnae.member.dto.request.MemberJoinRequest;
+import sillim.dongnae.member.dto.request.MemberLoginRequest;
+import sillim.dongnae.member.dto.response.MemberProfileResponse;
+import sillim.dongnae.member.entity.Member;
+import sillim.dongnae.member.servcie.MemberService;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/check-email")
+    public ResponseEntity<String> checkEmail(@RequestBody EmailCheckRequest request) {
+
+        boolean result = memberService.checkEmailDuplication(request.getEmail());
+        if (!result) {
+            return new ResponseEntity<>("존재하는 이메일입니다.", HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>("사용 가능한 이메일입니다.", HttpStatus.OK);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@RequestBody MemberJoinRequest request) {
+        boolean result = memberService.registerMember(request);
+        if (!result) {
+            return new ResponseEntity<>("존재하는 이메일입니다.", HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>("가입 완료!", HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+            @RequestBody MemberLoginRequest request,
+            HttpServletRequest servletRequest,
+            HttpServletResponse servletResponse
+    ) {
+
+        Member member = memberService.login(request);
+        if (member == null) {
+            return new ResponseEntity<>("존재하지 않습니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+        // 로그인 성공
+        // 세션 생성 및 사용자 정보 저장
+        HttpSession session = servletRequest.getSession();
+        session.setAttribute("memberId", member.getId());
+
+        // 세션 ID를 쿠키에 설정하여 클라이언트에 전달
+        Cookie sessionCookie = new Cookie("JSESSIONID", session.getId());
+        sessionCookie.setHttpOnly(true); // JS 에서 쿠키에 접근하지 못하도록 설정
+        sessionCookie.setPath("/"); // 전체 도메인에 대해 유효
+        servletResponse.addCookie(sessionCookie);
+
+        return new ResponseEntity<>(MemberProfileResponse.of(member), HttpStatus.OK);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile(HttpServletRequest servletRequest) {
+
+        HttpSession session = servletRequest.getSession(false);
+
+        if (session == null || session.getAttribute("memberId") == null) {
+            return new ResponseEntity<>("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+        Long memberId = (Long) session.getAttribute("memberId");
+
+        Member member = memberService.findById(memberId);
+        if (member == null) {
+            return new ResponseEntity<>("존재하지 않습니다.", HttpStatus.UNAUTHORIZED);
+        }
+
+        return new ResponseEntity<>(MemberProfileResponse.of(member), HttpStatus.OK);
+    }
+}
