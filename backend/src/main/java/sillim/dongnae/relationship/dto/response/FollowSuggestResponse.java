d888f1d1335b3c2ac359978e374b49aebb4cf4de
HamSeungChan <head0618@naver.com>
2024-11-08 14:46:13 +0900

    [refactor] : 생성자 매개변수로 객체 전달

--- backend/src/main/java/sillim/dongnae/relationship/dto/response/FollowSuggestResponse.java
+++ backend/src/main/java/sillim/dongnae/relationship/dto/response/FollowSuggestResponse.java
@@ -2,6 +2,7 @@ package sillim.dongnae.relationship.dto.response;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sillim.dongnae.member.entity.Member;
 
 @Getter
 @NoArgsConstructor
@@ -11,9 +12,9 @@ public class FollowSuggestResponse {
     private String email;
     private String name;
 
-    public FollowSuggestResponse(Long id, String email, String nickname) {
-        this.id = id;
-        this.email = email;
-        this.name = nickname;
+    public FollowSuggestResponse(Member member) {
+        this.id = member.getId();
+        this.email = member.getEmail();
+        this.name = member.getNickName();
     }
 }
