f158ccfd40345817734b7c2c4c6632d8cf0ba8b3
HamSeungChan <head0618@naver.com>
2024-11-14 16:39:30 +0900

    [feat] : ChatMessage 클래스 Entity로 변경 (#21)

--- backend/src/main/java/sillim/dongnae/chat/entity/ChatMessage.java
+++ backend/src/main/java/sillim/dongnae/chat/entity/ChatMessage.java
@@ -1,15 +1,35 @@
 package sillim.dongnae.chat.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sillim.dongnae.member.entity.Member;
 
 @Getter
+@Entity
 @NoArgsConstructor
 
 public class ChatMessage {
 
-    private String content;
-    private String writer;
-    private String roomId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id")
+    private ChatRoom chatRoom;
+
+
+    private String content;
+
+    public ChatMessage(Member writer, ChatRoom chatRoom, String content) {
+        this.writer = writer;
+        this.chatRoom = chatRoom;
+        this.content = content;
+    }
 }
